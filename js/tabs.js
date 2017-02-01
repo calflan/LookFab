598f9fc087c17cd7ce46879e3af3c7584f8cc8a1
Calum Flanagan <calumflanagan@Calums-MacBook-Pro.local>
2017-02-01 21:00:22 +0000

    Added Order Summary section, along with some colour changes to the forms.

--- js/tabs.js
+++ js/tabs.js
@@ -1,6 +1,7 @@
 document.getElementById('main').style.display='none';
 document.getElementById('cardholderAddr').style.display='none';
 document.getElementById('submitBtn').style.display='none';
+document.getElementById('orderSummary').style.display='none';
 
 
 
@@ -12,23 +13,29 @@ document.getElementById('submitBtn').style.display='none';
 
 // Make Card form appear and paypal/alipay disappear, when user clicks cardButton.
 document.getElementById('cardForm').style.display='none';
-
 document.getElementById('cardButton').onclick=function(){
+
   document.getElementById('main').style.display='';
   document.getElementById('cardholderAddr').style.display='';
   document.getElementById('submitBtn').style.display='';
+  document.getElementById('orderSummary').style.display='';
   document.getElementById('cardForm').style.display='';
+  
+  document.getElementById('cardholderMain').style.display='none';
   document.getElementById('paypal').style.display='none';
   document.getElementById('alipay').style.display='none';
 };
 
 // Make PayPal appear and card form/alipay disappear, when user clicks paypalButton.
 document.getElementById('paypal').style.display='none';
-
 document.getElementById('paypalButton').onclick=function(){
+
   document.getElementById('main').style.display='';
   document.getElementById('paypal').style.display='';
   document.getElementById('submitBtn').style.display='';
+  document.getElementById('orderSummary').style.display='';
+
+  document.getElementById('cardholderMain').style.display='none';
   document.getElementById('cardholderAddr').style.display='none';
   document.getElementById('cardForm').style.display='none';
   document.getElementById('alipay').style.display='none';
@@ -37,9 +44,15 @@ document.getElementById('paypalButton').onclick=function(){
 // Make AliPay appear and card form/paypal disappear, when user clicks alipayButton.
 document.getElementById('alipay').style.display='none';
 document.getElementById('alipayButton').onclick=function(){
+
+  //display these items
   document.getElementById('main').style.display='';
   document.getElementById('alipay').style.display='';
   document.getElementById('submitBtn').style.display='';
+  document.getElementById('orderSummary').style.display='';
+
+  //hide these items
+  document.getElementById('cardholderMain').style.display='none';
   document.getElementById('cardholderAddr').style.display='none';
   document.getElementById('cardForm').style.display='none';
   document.getElementById('paypal').style.display='none';
@@ -57,6 +70,7 @@ document.getElementById('alipayButton').onclick=function(){
 
 document.getElementById('newAddrForm').style.display='none';
 document.getElementById('newAddrButton').onclick=function(){
+  document.getElementById('cardholderMain').style.display='';
   document.getElementById('cardholderAddr').style.display='';
   document.getElementById('newAddrForm').style.display='';
   document.getElementById('useDeliveryAddr').style.display='none';
@@ -67,7 +81,8 @@ document.getElementById('useDeliveryAddr').style.display='none';
 
 document.getElementById('useDelAddrButton').onclick=function(){
   document.getElementById('cardholderAddr').style.display='';
-  document.getElementById('newAddrForm').style.display='none';
+  document.getElementById('cardholderMain').style.display='';
   document.getElementById('useDeliveryAddr').style.display='';
+  document.getElementById('newAddrForm').style.display='none';
 };
 
